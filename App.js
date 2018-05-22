64a3757e61279ebbf1601c5ee995b239b5e3e10a
Varun Nath <nathvarun@hotmail.com>
2018-05-22 19:43:28 +0530

    Added master branch with instructions and readme

--- App.js
+++ App.js
@@ -2,67 +2,54 @@ import React, { Component } from "react";
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  FlatList
 } from "react-native";
 
-import { createBottomTabNavigator } from 'react-navigation'
-import Icon from 'react-native-vector-icons/Ionicons'
-
-export class Home extends Component {
+class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open Up App.js to start working with your app</Text>
+        <Text style={styles.title}>1. Checkout the Tutorial branch that you want to view!</Text>
+        <View style={styles.codeView}>
+          <Text style={styles.code}>$ git branch --all</Text>
+          <Text style={styles.code}>* master</Text>
+          <Text style={styles.code}>* remotes/origin/1-stacknavigator</Text>
+          <Text style={styles.code}>* remotes/origin/2-tabnavigator</Text>
+        </View>
+        <Text style={styles.title}>2. To view the first tutorial</Text>
+        <View style={styles.codeView}>
+          <Text style={styles.code}>$ git checkout 1-stacknavigator</Text>
+        </View>
+        <Text style={styles.title}>3. Refresh the Simulator!</Text>
+
+
       </View>
     );
   }
 }
-export class Settings extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Settings</Text>
-      </View>
-    );
-  }
-}
-export default createBottomTabNavigator({
-  Home: {
-    screen: Home,
-    navigationOptions: {
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-home" color={tintColor} size={24} />
-      )
-    }
-  },
-  Settings: {
-    screen: Settings,
-    navigationOptions: {
-      tabBarLabel: 'Settings',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-settings" color={tintColor} size={24} />
-      )
-    }
-  }
-
-}, {//router config
-    initialRouteName: 'Settings',
-    order: ['Settings', 'Home'],
-    //navigation for complete tab navigator
-    navigationOptions: {
-      tabBarVisible: true
-    },
-    tabBarOptions: {
-      activeTintColor: 'red',
-      inactiveTintColor: 'grey'
-    }
-  });
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  title: {
+    alignSelf: 'flex-start', paddingHorizontal: 20,
+    fontWeight: "700",
+
+  },
+  codeView: {
+    width: "90%",
+    borderRadius: 5,
+    margin: 20,
+    padding: 20,
+    backgroundColor: '#eaeaea'
+  },
+  code: {
+    fontWeight: "400",
+    fontSize: 15
   }
 });
