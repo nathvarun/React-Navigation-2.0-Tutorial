27e7329fca6ce219e1ab363cc3b796bd6aeaf4a4
Varun Nath <nathvarun@hotmail.com>
2018-05-22 18:52:22 +0530

    2. TabNavigator Tutorial

--- App.js
+++ App.js
@@ -1,28 +1,68 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from 'react-navigation'
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet
+} from "react-native";
 
-import LoginScreen from './screens/LoginScreen'
-import HomeScreen from './screens/HomeScreen'
-export default class App extends React.Component {
+import { createBottomTabNavigator } from 'react-navigation'
+import Icon from 'react-native-vector-icons/Ionicons'
+
+export class Home extends Component {
   render() {
     return (
-      <AppStackNavigator />
+      <View style={styles.container}>
+        <Text>Open Up App.js to start working with your app</Text>
+      </View>
     );
   }
 }
+export class Settings extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Settings</Text>
+      </View>
+    );
+  }
+}
+export default createBottomTabNavigator({
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: 'Home',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-home" color={tintColor} size={24} />
+      )
+    }
+  },
+  Settings: {
+    screen: Settings,
+    navigationOptions: {
+      tabBarLabel: 'Settings',
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="ios-settings" color={tintColor} size={24} />
+      )
+    }
+  }
 
-const AppStackNavigator = createStackNavigator({
-  Login: LoginScreen,
-  Home: HomeScreen
-
-})
+}, {//router config
+    initialRouteName: 'Settings',
+    order: ['Settings', 'Home'],
+    //navigation for complete tab navigator
+    navigationOptions: {
+      tabBarVisible: true
+    },
+    tabBarOptions: {
+      activeTintColor: 'red',
+      inactiveTintColor: 'grey'
+    }
+  });
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
 });
