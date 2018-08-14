0cb51f65d332d216bf7af463a4014226b3ed3b58
Varun Nath <nathvarun@hotmail.com>
2018-08-14 10:19:46 +0530

    Added Drawer Navigator files

--- App.js
+++ App.js
@@ -2,60 +2,98 @@ import React, { Component } from "react";
 import {
   View,
   Text,
-  StyleSheet
+  StyleSheet,
+  Platform,
+  StatusBar,
+  SafeAreaView,
+  Image
 } from "react-native";
+import { createBottomTabNavigator, createMaterialTopTabNavigator, createDrawerNavigator, DrawerItems } from 'react-navigation'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 import Icon from 'react-native-vector-icons/Ionicons'
+import { Container, Content, Header, Body } from 'native-base'
+
+const CustomDrawerContentComponent = (props) => (
+
+  <Container>
+    <SafeAreaView style={styles.safeArea}>
+
+      <Header style={styles.drawerHeader}>
+        <Body>
+          <Image
+            style={styles.drawerImage}
+            source={require('./assets/Unsure-Programmer-Logo.png')} />
+        </Body>
+      </Header>
+
+      <Content>
+        <DrawerItems {...props} />
+      </Content>
+    </SafeAreaView>
+
+  </Container>
+
+);
+
+export default class App extends Component {
+  render() {
+    return (
+      <AppDrawerNavigator />
+    )
+  }
+}
 class HomeScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Tab Navigator Tutorial 2!</Text>
+        <Text>Home</Text>
       </View>
     );
   }
 }
+
 class SettingsScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Settings</Text>
+
       </View>
-    );
+    )
   }
 }
-
-
-export default createMaterialBottomTabNavigator({
-  Home: {
-    screen: HomeScreen,
-    navigationOptions: {
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-home" color={tintColor} size={24} />
-      )
-    }
-  },
-  Settings: {
-    screen: SettingsScreen,
-    navigationOptions: {
-      tabBarLabel: 'Home',
-      tabBarIcon: ({ tintColor }) => (
-        <Icon name="ios-settings" color={tintColor} size={24} />
-      )
-    }
-  }
+const AppDrawerNavigator = createDrawerNavigator({
+  Home: HomeScreen,
+  Settings: SettingsScreen
 }, {
     initialRouteName: 'Home',
-    // order: ['Settings', 'Home'],
-    activeTintColor: 'white',
-    shifting: true
-  })
+    drawerPosition: 'left',
+    contentComponent: CustomDrawerContentComponent,
+    drawerOpenRoute: 'DrawerOpen',
+    drawerCloseRoute: 'DrawerClose',
+    drawerToggleRoute: 'DrawerToggle',
+
+  });
 
 const styles = StyleSheet.create({
+
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'white'
+  },
+
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  drawerHeader: {
+    height: 200,
+    backgroundColor: 'white'
+  },
+  drawerImage: {
+    height: 150,
+    width: 150,
+    borderRadius: 75
   }
 });
